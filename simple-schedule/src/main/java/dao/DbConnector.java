919b83f717ff6c14cfe3757fef2564b6ca7d5fe5
jgeis <jgeis@VENGEANCE>
2021-04-26 16:32:12 -0400

    expanded connect() to get properties from config.properties

--- simple-schedule/src/main/java/dao/DbConnector.java
+++ simple-schedule/src/main/java/dao/DbConnector.java
@@ -1,6 +1,8 @@
 package dao;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,26 +16,22 @@ public class DbConnector {
 	private String user;
 	private String password;
 	
-	// get url, user, and password from config.properties
-	try {
-		FileReader reader = new FileReader(
-				"C:\\Users\\jgeis\\eclipse-workspace\\simple-calculator\\src\\main\\java\\jdbc\\config.properties");
-		
-		Properties p = new Properties();
-		p.load(reader);
-		
-		user = p.getProperty("DB_USER");
-		password = p.getProperty("DB_PASSWORD");
-		url = p.getProperty("DB_URL");
-	} catch (Exception e) {
-		// TODO: handle exception
-	}
 	
 	
 	// connect method
 	private static Connection con;
 	public Connection connect() {
 		try {
+			FileReader reader = new FileReader(
+					"C:\\Users\\jgeis\\git\\revature-simple-calendar\\simple-schedule\\src\\main\\resources\\config.properties");
+			
+			Properties p = new Properties();
+			p.load(reader);
+			
+			user = p.getProperty("DB_USER");
+			password = p.getProperty("DB_PASSWORD");
+			url = p.getProperty("DB_URL");
+			
 			System.out.println("Connecting...");
 //			System.out.println(url);
 //			System.out.println(user);
@@ -41,8 +39,16 @@ public class DbConnector {
 			con = DriverManager.getConnection(url,user,password);
 			System.out.println("Connected");
 			return con;
-		}catch(SQLException e) {
+		} catch(SQLException e) {
 			System.out.println("Issue connecting to DB");
+			return con;
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return con;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 			return con;
 		}
 	}
